4814ee8855f25e5d6a274fca66f8df1f4bb6f8ec
mungekarkiran <mungekarkiran05@gmail.com>
2024-01-07 14:20:27 +0530

    modify stage_03_data_transformation.py file

--- src/red_wine_quality_project/pipeline/stage_03_data_transformation.py
+++ src/red_wine_quality_project/pipeline/stage_03_data_transformation.py
@@ -9,10 +9,26 @@ class DataTransformationTrainingPipeline:
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_spliting()
+        # config = ConfigurationManager()
+        # data_transformation_config = config.get_data_transformation_config()
+        # data_transformation = DataTransformation(config=data_transformation_config)
+        # data_transformation.train_test_spliting()
+
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                status = f.read().split(" ")[-1]
+
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_spliting()
+
+            else:
+                raise Exception("You data schema is not valid")
+
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
