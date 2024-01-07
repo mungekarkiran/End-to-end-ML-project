3778e6ec8e13f639073ed588ef21cdc96491c14d
mungekarkiran <mungekarkiran05@gmail.com>
2024-01-07 14:14:52 +0530

    Data transformation component

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 from red_wine_quality_project import logger
 from red_wine_quality_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from red_wine_quality_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from red_wine_quality_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -28,3 +29,13 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = DataTransformationTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
