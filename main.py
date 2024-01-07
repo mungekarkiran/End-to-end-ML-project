229a28b858ddc1fa51c89fda27b8979e3851ace7
mungekarkiran <mungekarkiran05@gmail.com>
2024-01-07 12:09:53 +0530

    Data ingestion

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 # from src.red_wine_quality_project.logging import logger
 # logger.info("This is our custom log!")
 
+# from red_wine_quality_project import logger
+# logger.info("My name is Kiran!")
+
+
 from red_wine_quality_project import logger
-logger.info("My name is Kiran!")
+from red_wine_quality_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
