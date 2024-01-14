409d9d9754d3ad727c5493c4b0bc3e7b62c381ce
mungekarkiran <mungekarkiran05@gmail.com>
2024-01-14 21:40:48 +0530

    Model evaluation component

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from red_wine_quality_project.pipeline.stage_01_data_ingestion import DataIngest
 from red_wine_quality_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from red_wine_quality_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from red_wine_quality_project.pipeline.stage_04_model_trainer import ModelTrainerPipeline
+from red_wine_quality_project.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -45,6 +46,16 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    obj = ModelTrainerPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = ModelEvaluationPipeline()
    obj.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
